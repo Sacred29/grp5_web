ecec46753114805de4869043ec4064bf79d38dd1
dennytan-19 <54209319+dennytan-19@users.noreply.github.com>
2024-03-30 14:11:40 +0800

    fix js errors, try out recaptcha in register page

--- process_register.php
+++ process_register.php
@@ -1,21 +1,51 @@
 <?php
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-    $fname = $lname = $email = $pwd = $errorMsg = $errorMsg2 = $errorMsg3 = "";
-    $singleError = true; // Initialize as true
-    $fields = array(
-        "fname" => "First name",
-        "lname" => "Last name",
-        "email" => "Email",
-        "pwd" => "Password",
-        "pwd_confirm" => "Password Confirmation"
-    );
-    $errorMsg = "";
-    $success = true;
-    $pwd= $_POST['pwd'];
-    $pwd_confirm = $_POST['pwd_confirm'];
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$fname = $lname = $email = $pwd = $errorMsg = $errorMsg2 = $errorMsg3 = "";
+$is_bot = true;
+$userPrivilege = "user";
 
+$singleError = true; // Initialize as true
+$fields = array(
+    "fname" => "First name",
+    "lname" => "Last name",
+    "email" => "Email",
+    "pwd" => "Password",
+    "pwd_confirm" => "Password Confirmation"
+);
+$errorMsg = "";
+$success = true;
+$pwd = $_POST['pwd'];
+$pwd_confirm = $_POST['pwd_confirm'];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['recaptcha_response'])) {
+    // Build POST request:
+    $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+    $recaptcha_secret = '6Le0e7MZAAAAAI-pCmb3uScqvJUf5y6RN6bTqra4sc';
+    $recaptcha_response = $_POST['recaptcha_response']; // replace with getenv
+
+    // Make and decode POST request:
+    $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
+    $recaptcha = json_decode($recaptcha);
+
+    // check if recaptcha server-side validation is successful
+    if ($recaptcha->success) {
+        // Take action based on the score returned:
+        if ($recaptcha->score >= 0.5) {
+            $is_bot = false;
+            echo "Human";
+        } else {
+            $errorMsg = "reCAPTCHA thinks you are a bot. Try again in a few minutes.";
+            echo "Bot";
+        }
+    } else {
+        $errorMsg .= "Oops! Something went wrong with reCAPTCHA verification.";
+        $success = false;
+    }
+}
+
+if (!$is_bot) {
     foreach ($fields as $field => $fieldname) {
         if (empty($_POST[$field])) {
             if ($singleError) {
@@ -25,9 +55,7 @@
                 $errorMsg .= "<br>" . $fieldname . " is required.<br>";
             }
             $success = false;
-        }
-        else
-        {
+        } else {
             $email = sanitize_input($_POST["email"]);
             // Additional check to make sure e-mail address is well-formed.
             $lname = sanitize_input($_POST["lname"]);
@@ -37,14 +65,12 @@
                 $errorMsg2 .= "Invalid email format.";
                 $success = false;
             }
-       }
+        }
     }
-
     if ($pwd !== $pwd_confirm) {
         $errorMsg .= "Passwords do not match.";
         $success = false;
-    }
-    else {
+    } else {
         $pwd = password_hash($_POST["pwd"], PASSWORD_DEFAULT);
     }
 
@@ -52,36 +78,32 @@
         $errorMsg .= "Invalid last name format.";
         $success = false;
     }
+}
 
-    if ($success)
-        {
-            //$hashedPassword = password_hash($pwd, PASSWORD_DEFAULT);
-            echo "<h4>Registration successful!</h4>";
-            echo "<p>Email: " . $email;
-            echo "<p>First name: " . $fname;
-            echo "<p>Last name: " . $lname;
-            echo "<p>Hashed password: " .$pwd;
-            saveMemberToDB();
+if ($success) {
+    //$hashedPassword = password_hash($pwd, PASSWORD_DEFAULT);
+    echo "<h4>Registration successful!</h4>";
+    echo "<p>Email: " . $email;
+    echo "<p>First name: " . $fname;
+    echo "<p>Last name: " . $lname;
+    echo "<p>Hashed password: " . $pwd;
+    saveMemberToDB();
+} else {
+    echo "<h4>The following errors were detected:</h4>";
+    echo "<p>" . $errorMsg2 . "</p>";
+    echo "<p>" . $errorMsg . "</p>";
+}
 
-        }
-    else
-        {
-            echo "<h4>The following errors were detected:</h4>";
-            echo "<p>" . $errorMsg2 . "</p>";
-            echo "<p>" . $errorMsg . "</p>";
-        }
-    
-    
 /*
 * Helper function that checks input for malicious or unwanted content.
 */
-    function sanitize_input($data)
-    {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
+function sanitize_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 
 /*
     Helper function to write member data to database
@@ -89,56 +111,51 @@
     --> how to write to dabase using PHP oo MySSQLi
 */
 
-    function saveMemberToDB() {
-        global $fname, $lname, $email, $pwd, $errorMsg, $success;
+function saveMemberToDB()
+{
+    global $fname, $lname, $email, $pwd, $userPrivilege, $errorMsg, $success;
+    //create db connection
+    $config_file = '/var/www/private/db-config.ini';
 
-        //create db connection
-        $config_file = '/var/www/private/db-config.ini';
+    if (file_exists($config_file)) {
+        // Parse the INI file
+        $config = parse_ini_file($config_file);
+    } else {
+        // Get configuration from environment variables
+        $config['servername'] = getenv('SERVERNAME');
+        $config['username'] = getenv('DB_USERNAME');
+        $config['password'] = getenv('DB_PASSWORD');
+        $config['dbname'] = getenv('DBNAME');
+    }
 
-        if (file_exists($config_file)) {
-         // Parse the INI file
-            $config = parse_ini_file($config_file);
-        } else {
-            // Get configuration from environment variables
-            $config['servername'] = getenv('SERVERNAME');
-            $config['username'] = getenv('DB_USERNAME');
-            $config['password'] = getenv('DB_PASSWORD');
-            $config['dbname'] = getenv('DBNAME');
-        }
-        
-        $conn = new mysqli(
-            getenv('SERVERNAME'),
-            getenv('DB_USERNAME'), 
-            getenv('DB_PASSWORD'), 
-            getenv('DBNAME')
-        ); 
+    $conn = new mysqli(
+        getenv('SERVERNAME'),
+        getenv('DB_USERNAME'),
+        getenv('DB_PASSWORD'),
+        getenv('DBNAME')
+    );
 
+    if ($conn->connect_error) {
+        $errorMsg = "Connection failed: " . $conn->connect_error;
+        $success = false;
+    } else {
+
+        //check connection
         if ($conn->connect_error) {
-            $errorMsg = "Connection failed: " .$conn->connect_error;
-            $success = false;
-        }
-        else {
-        
-            //check connection
-            if ($conn->connect_error){
-               
-            }
-            else {
-                //Prepare statement
-                //Bind and execute query statement
-                $stmt = $conn->prepare("INSERT INTO userTable (fName, lName, email, password, userPrivilege) VALUES (?,?,?,?, 'user')");
-                $stmt->bind_param("ssss", $fname, $lname, $email, $pwd);
-                
-                //$stmt = $conn->prepare("INSERT INTO world_of_pets_members (fname, lname, email, password) VALUES ('jane','doe','jane@abc.com','123')");
+        } else {
+            //Prepare statement
+            //Bind and execute query statement
+            $stmt = $conn->prepare("INSERT INTO userTable (fName, lName, email, password, userPrivilege) VALUES (?,?,?,?, 'user')");
+            $stmt->bind_param("ssss", $fname, $lname, $email, $pwd);
 
-                if (!$stmt->execute()){
-                    $errorMsg = "Execute failed: (" .$stmt->errno .") " . $stmt->error;
-                    $success = false;
-                }
-                $stmt->close();
-            }
-            $conn->close();
-        }
-     }
-?>
+            //$stmt = $conn->prepare("INSERT INTO world_of_pets_members (fname, lname, email, password) VALUES ('jane','doe','jane@abc.com','123')");
 
+            if (!$stmt->execute()) {
+                $errorMsg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+                $success = false;
+            }
+            $stmt->close();
+        }
+        $conn->close();
+    }
+}
--- register.php
+++ register.php
@@ -1,52 +1,67 @@
 <?php
-    include "inc/head.inc.php";
+include "inc/head.inc.php";
 ?>
+
 <body>
     <?php
     include "inc/nav.inc.php";
     ?>
-<main class="container">
-    <h1>Member Registration</h1>
-<p>
-    For existing members, please go to the
-    <a href="login.php">Sign In page</a>.
-</p>
-<form action="process_register.php" method="post">
-<div class="mb-3">
-    <label for="fname" class="form-label">First Name:</label>
-    <input maxlength="45" type="text" id="fname" name="fname" class="form-control" placeholder="Enter first name">
-</div>
-<div class="mb-3">
-    <label for="lname" class="form-label">Last Name:</label>
-    <input required maxlength="45" type="text" id="lname" name="lname" class="form-control" placeholder="Enter last name">
-    <!-- <input id="lname" name="lname" class="form-control" placeholder="Enter last name"> -->
-</div>
-<div class="mb-3">
-    <label for="email" class="form-label">Email:</label>
-    <input required maxlength="45" type="email" id="email" name="email" class="form-control" placeholder="Enter email">
-    <!-- <input id="email" name="email" class="form-control" placeholder="Enter email"> -->
-</div>
-<div class="mb-3">
-    <label for="pwd" class="form-label">Password:</label>
-    <input required type="password" id="pwd" name="pwd" class="form-control" placeholder="Enter password">
-    <!-- <input id="pwd" name="pwd" class="form-control" placeholder="Enter password"> -->
-</div>
-<div class="mb-3">
-    <label for="pwd_confirm" class="form-label">Confirm Password:</label>
-    <input required type="password" id="pwd_confirm" name="pwd_confirm"  class="form-control"placeholder="Confirm password">
-    <!-- <input id="pwd_confirm" name="pwd_confirm"  class="form-control"placeholder="Confirm password"> -->
-</div>
-<div class="mb-3 form-check">
-    <input required type="checkbox" name="agree" class="form-check-input"> 
-    <label class="form-check-label" for="agree">
-    Agree to terms and conditions.</label>
-</div>
-<div class="mb-3">
-    <button type="submit">Submit</button>
-</div>
-</form>
-</main>
+    <main class="container">
+        <h1>Member Registration</h1>
+        <p>
+            For existing members, please go to the
+            <a href="login.php">Sign In page</a>.
+        </p>
+        <form id="register-form" action="process_register.php" method="post">
+            <div class="mb-3">
+                <label for="fname" class="form-label">First Name:</label>
+                <input maxlength="45" type="text" id="fname" name="fname" class="form-control" placeholder="Enter first name">
+            </div>
+            <div class="mb-3">
+                <label for="lname" class="form-label">Last Name:</label>
+                <input required maxlength="45" type="text" id="lname" name="lname" class="form-control" placeholder="Enter last name">
+                <!-- <input id="lname" name="lname" class="form-control" placeholder="Enter last name"> -->
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email:</label>
+                <input required maxlength="45" type="email" id="email" name="email" class="form-control" placeholder="Enter email">
+                <!-- <input id="email" name="email" class="form-control" placeholder="Enter email"> -->
+            </div>
+            <div class="mb-3">
+                <label for="pwd" class="form-label">Password:</label>
+                <input required type="password" id="pwd" name="pwd" class="form-control" placeholder="Enter password">
+                <!-- <input id="pwd" name="pwd" class="form-control" placeholder="Enter password"> -->
+            </div>
+            <div class="mb-3">
+                <label for="pwd_confirm" class="form-label">Confirm Password:</label>
+                <input required type="password" id="pwd_confirm" name="pwd_confirm" class="form-control" placeholder="Confirm password">
+                <!-- <input id="pwd_confirm" name="pwd_confirm"  class="form-control"placeholder="Confirm password"> -->
+            </div>
+            <div class="mb-3 form-check">
+                <input required type="checkbox" name="agree" class="form-check-input">
+                <label class="form-check-label" for="agree">
+                    Agree to terms and conditions.</label>
+            </div>
+            <div class="mb-3">
+                <input type="hidden" name="recaptcha_response" id="recaptchaResponse">
+                <button type="submit">Submit</button>
+            </div>
+        </form>
+    </main>
     <?php
     include "inc/footer.inc.php";
     ?>
 </body>
+<script>
+    grecaptcha.ready(() => {
+        document.getElementById('register-form').addEventListener("submit", function(event) {
+            event.preventDefault();
+            grecaptcha.execute('6Le0e7MZAAAAAJDAnFTrhlM8DJ1u-Fvi3N702bD7', {
+                action: 'signup'
+            }).then(token => {
+                document.querySelector('#recaptchaResponse').value = token;
+                document.getElementById('register-form').submit();
+            });
+        }, false);
+    });
+</script>
