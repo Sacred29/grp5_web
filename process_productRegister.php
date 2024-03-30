c632191b307f17b1b07f29b1780a44fbc3e3566e
xwlow <2301949@sit.singaporetech.edu.sg>
2024-03-31 00:09:29 +0800

    productthings

---
+++ process_productRegister.php
@@ -0,0 +1,128 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$productName = $arrivalDate = $genre = $bookUEN = $price = $bookAuthor = $bookPublisher = $productImage = $errorMsg = $errorMsg2 = $errorMsg3 = "";
+$singleError = true; // Initialize as true
+$fields = array(
+    "productName" => "Product Name",
+    "arrivalDate" => "Arrival Date",
+    "genre" => "Genre",
+    "bookUEN" => "Book UEN",
+    "price" => "Price",
+    "bookAuthor" => "Book Author",
+    "bookPublisher" => "Book Publisher",
+    "productImage" => "Product Image",
+);
+$errorMsg = "";
+$success = true;
+
+foreach ($fields as $field => $fieldname) {
+    if (empty($_POST[$field])) {
+        if ($singleError) {
+            $errorMsg = $fieldname . " is required.";
+            $singleError = false; // Set to false if multiple fields are missing
+        } else {
+            $errorMsg .= "<br>" . $fieldname . " is required.<br>";
+        }
+        $success = false;
+    } else {
+        $productName = sanitize_input($_POST["productName"]);
+        // Additional check to make sure e-mail address is well-formed.
+        $arrivalDate = sanitize_input($_POST["arrivalDate"]);
+        $genre = sanitize_input($_POST["genre"]);
+        $bookUEN = sanitize_input($_POST["bookUEN"]);
+        $price = sanitize_input($_POST["price"]);
+        $bookAuthor = sanitize_input($_POST["bookAuthor"]);
+        $bookPublisher = sanitize_input($_POST["bookPublisher"]);
+        $productImage = sanitize_input($_POST["productImage"]);
+    }
+}
+
+
+if ($success) {
+    //$hashedPassword = password_hash($pwd, PASSWORD_DEFAULT);
+    echo "<h4>Product Registration Successful!</h4>";
+    echo "<p>Product Name: " . $productName;
+    echo "<p>Arrival Date: " . $arrivalDate;
+    echo "<p>Genre: " . $genre;
+    echo "<p>Price: " . $price;
+    echo "<p>Book Author: " . $bookAuthor;
+    echo "<p>Book Publisher: " . $bookPublisher;
+    echo "<p>Product Image: " . $productImage;
+    
+    saveProductToDB();
+} else {
+    echo "<h4>The following errors were detected:</h4>";
+    echo "<p>" . $errorMsg2 . "</p>";
+    echo "<p>" . $errorMsg . "</p>";
+}
+
+
+/*
+* Helper function that checks input for malicious or unwanted content.
+*/
+function sanitize_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+/*
+    Helper function to write member data to database
+    Function --> retrieve database login from config
+    --> how to write to dabase using PHP oo MySSQLi
+*/
+
+function saveProductToDB()
+{
+    global $productName, $arrivalDate, $genre, $bookUEN, $price, $bookAuthor, $bookPublisher, $productImage, $errorMsg, $success;
+
+    $productImage = "/images/" . $productImage; 
+    //create db connection
+    $config_file = '/var/www/private/db-config.ini';
+
+    if (file_exists($config_file)) {
+        // Parse the INI file
+        $config = parse_ini_file($config_file);
+    } else {
+        // Get configuration from environment variables
+        $config['servername'] = getenv('SERVERNAME');
+        $config['username'] = getenv('DB_USERNAME');
+        $config['password'] = getenv('DB_PASSWORD');
+        $config['dbname'] = getenv('DBNAME');
+    }
+
+    $conn = new mysqli(
+        getenv('SERVERNAME'),
+        getenv('DB_USERNAME'),
+        getenv('DB_PASSWORD'),
+        getenv('DBNAME')
+    );
+
+    if ($conn->connect_error) {
+        $errorMsg = "Connection failed: " . $conn->connect_error;
+        $success = false;
+    } else {
+
+        //check connection
+        if ($conn->connect_error) {
+        } else {
+            //Prepare statement
+            //Bind and execute query statement
+            $stmt = $conn->prepare("INSERT INTO productTable (productName, arrivalDate, productGenre, bookUEN, price, bookAuthor, bookPublisher, productImage) VALUES (?,?,?,?,?,?,?,?)");
+            $stmt->bind_param("ssssdsss", $productName, $arrivalDate, $genre, $bookUEN, $price, $bookAuthor, $bookPublisher, $productImage);
+
+            //$stmt = $conn->prepare("INSERT INTO world_of_pets_members (fname, lname, email, password) VALUES ('jane','doe','jane@abc.com','123')");
+
+            if (!$stmt->execute()) {
+                $errorMsg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+                $success = false;
+            }
+            $stmt->close();
+        }
+        $conn->close();
+    }
+}
--- products.php
+++ products.php
@@ -2,61 +2,6 @@
 <html lang="en">
 <?php
 include "inc/head.inc.php";
-//create db connection
-$config_file = parse_ini_file('/var/www/private/db-config.ini');
-if (file_exists($config_file)) {
-    // Parse the INI file
-    echo ("hellooo");
-    $config = parse_ini_file($config_file);
-} else {
-    // Get configuration from environment variables
-    echo ("hello");
-    $config['servername'] = getenv('SERVERNAME');
-    $config['username'] = getenv('DB_USERNAME');
-    $config['password'] = getenv('DB_PASSWORD');
-    $config['dbname'] = getenv('DBNAME');
-}
-
-$conn = new mysqli(
-    $config['servername'],
-    $config['username'],
-    $config['password'],
-    $config['dbname']
-);
-if ($conn->connect_error) {
-    $errorMsg = "Connection failed: " . $conn->connect_error;
-    $success = false;
-} else {
-
-
-
-    //check connection
-    if ($conn->connect_error) {
-        $errorMsg = "Connection failed: " . $conn->connect_error;
-        $success = false;
-    } else {
-        //Prepare statement
-        //Bind and execute query statement
-        $stmt = $conn->query("SELECT * FROM productTable");
-
-        //$stmt = $conn->prepare("INSERT INTO world_of_pets_members (fname, lname, email, password) VALUES ('jane','doe','jane@abc.com','123')");
-
-
-
-        // Check if there are rows returned
-        if ($stmt->num_rows > 0) {
-            // Loop through the rows and fetch the data
-            while ($row = $stmt->fetch_assoc()) {
-                // Access data using column names
-                echo "ID: " . $row["productID"] . " - Name: " . $row["productName"] . "<br>";
-                // Adjust column names as per your table structure
-            }
-        } else {
-            echo "0 results";
-        }
-    }
-    $conn->close();
-}
 
 ?>
 
@@ -74,6 +19,9 @@ if ($conn->connect_error) {
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
+                        <a href="createProduct.php" target="">
+                            <button class="btn btn-primary">Register New Product</button>
+                        </a>
                         <div class="section-heading">
                             <!-- <span>Featured Products</span> -->
                             <h2>New Releases</h2>
@@ -81,7 +29,87 @@ if ($conn->connect_error) {
                     </div>
                 </div>
                 <div class="row">
-                    <?php foreach ($books as $book) { ?>
+                    <?php
+                    $books = [];
+                    //create db connection
+                    $config_file = parse_ini_file('/var/www/private/db-config.ini');
+                    if (file_exists($config_file)) {
+                        // Parse the INI file
+
+                        $config = parse_ini_file($config_file);
+                    } else {
+                        // Get configuration from environment variables
+                        $config['servername'] = getenv('SERVERNAME');
+                        $config['username'] = getenv('DB_USERNAME');
+                        $config['password'] = getenv('DB_PASSWORD');
+                        $config['dbname'] = getenv('DBNAME');
+                    }
+
+                    $conn = new mysqli(
+                        $config['servername'],
+                        $config['username'],
+                        $config['password'],
+                        $config['dbname']
+                    );
+                    if ($conn->connect_error) {
+                        $errorMsg = "Connection failed: " . $conn->connect_error;
+                        $success = false;
+                    } else {
+
+
+
+                        //check connection
+                        if ($conn->connect_error) {
+                            $errorMsg = "Connection failed: " . $conn->connect_error;
+                            $success = false;
+                        } else {
+                            //Prepare statement
+                            //Bind and execute query statement
+                            $stmt = $conn->query("SELECT * FROM productTable");
+
+                            //$stmt = $conn->prepare("INSERT INTO world_of_pets_members (fname, lname, email, password) VALUES ('jane','doe','jane@abc.com','123')");
+
+
+
+                            // Check if there are rows returned
+                            if ($stmt->num_rows > 0) {
+                                // Loop through the rows and fetch the data
+                                while ($row = $stmt->fetch_assoc()) {
+                                    // Access data using column names
+                                    $books[] = $row;
+                                    // Adjust column names as per your table structure
+                                }
+                            } else {
+                                echo "0 results";
+                            }
+                        }
+                        $conn->close();
+                    }
+
+                    echo "<section>";
+                    foreach ($books as $book) {
+                        $bookPrice = number_format((float)$book['price'],2);
+                        echo "<div class= 'col-md-4 col-sm-6 col-xs-12'>";
+                        echo "<div class='featured-item'>";
+                        echo "<div class='thumb'>";
+                        echo "<img src='{$book['productImage']}' alt=''>";
+                        echo "</div>";
+                        echo "<div class='down-content'>";
+                        echo "<h4>Product Name: " . $book['productName'] . "</h4></br>";
+                        echo "<span><sup>Price: " . $bookPrice . "</span></sup></br>";
+                        echo "<p>Product Author: " . $book['bookAuthor'] . "</p</br>";
+                        echo "<p>Product Publisher: " . $book['bookPublisher'] . "</p</br>";
+                        echo "<div class='text-button' data-toggle='modal' data-target='#myModal'>";
+                        echo "<a href='#'>View More</a>";
+                        echo "</div>";
+                        echo "</div>";
+                        echo "</div>";
+                        echo "</div>";
+                    }
+                    echo "</section>";
+
+                    /*
+                        ?>
                         <div class="col-md-4 col-sm-6 col-xs-12">
                             <div class="featured-item">
                                 <div class="thumb">
@@ -104,7 +132,7 @@ if ($conn->connect_error) {
                                 </div>
                             </div>
                         </div>
-                    <?php } ?>
+
 
                     <div class="col-md-4 col-sm-6 col-xs-12">
                         <div class="featured-item">
@@ -194,6 +222,7 @@ if ($conn->connect_error) {
 
                                 <p>This is an image about tabby cats.</p>
 
+
                                 <div class="text-button">
                                     <a href="product-details.html">View More</a>
                                 </div>
@@ -202,7 +231,8 @@ if ($conn->connect_error) {
                     </div>
                 </div>
             </div>
-        </section>
+        </section>*/
+                    ?>
 
     </main>
     <?php
@@ -214,6 +244,39 @@ if ($conn->connect_error) {
     <div id="imgModal" class="imgModal">
         <span class="close">&times;</span>
         <img class="modal-content" id="img01">
+    </div>
+
+    <!-- Modal -->
+    <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <?php
+                    foreach ($books as $book) {
+
+                        echo "Product Name: " . $book['productName'] . "</br>";
+                    }
+                    ?>
+                </div>
+                <div class="modal-body">
+                    <?php
+
+
+                    echo "<p>Price: " . $bookPrice . "</p>";
+                    echo "<p>Product Author: " . $book['bookAuthor'] . "</p</br>";
+                    echo "<p>Product Publisher: " . $book['bookPublisher'] . "</p</br>";
+
+                    ?>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+
+        </div>
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js" type="text/javascript"></script>
