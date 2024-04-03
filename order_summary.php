cc2ff5c1d6ae2c49b33d1eccc5c629f00daf6375
dennytan-19 <54209319+dennytan-19@users.noreply.github.com>
2024-04-04 02:09:47 +0800

    implement order summary, stripe payment and moving of cart items to db after payment success

--- order_summary.php
+++ order_summary.php
@@ -4,43 +4,55 @@ include "inc/head.inc.php";
 
 <body>
     <?php
-    session_start();
     include "inc/nav.inc.php";
+    session_start();
+    // Check if the user is logged in
+    if (!isset($_SESSION['user_privilege'])) {
+        // Redirect to login page if not logged in
+        header('Location: login.php');
+        exit;
+    }
     ?>
     <main class="container">
-        <h1> Checkout </h1>
+        <h1> Order summary </h1>
         <?php
-        if (isset($_SESSION["cart_item"])) {
-            $cartItems = $_SESSION["cart_item"];
-            $total_quantity = 0;
-            $total_price = 0;
-            echo "<script> console.log('Cart Items: " . json_encode($cartItems) . "');  </script>";
+        $total_quantity = 0;
+        $total_price = 0;
+
+        $order_books = [];
+        if (!empty($_SESSION["cart_item"])) {
+            // Loop through the rows and fetch the data
+            foreach ($_SESSION["cart_item"] as $item) {
+                $order_books[] = $item;
+            }
+        } else {
+            echo "The cart is empty";
+        }
 
         ?>
-
+        <div class="overflow-auto">
             <table class="tbl-cart" cellpadding="10" cellspacing="1">
                 <tbody>
                     <tr>
                         <th style="text-align:left;">Name</th>
                         <!--<th style="text-align:center;">Code</th>-->
                         <th style="text-align:center;" width="10%">Unit Price</th>
-                        <th style="text-align:center;" width="10%">Quantity</th>
+                        <th style="text-align:center;" width="10%">Quantity ordered</th>
                         <th style="text-align:center;" width="10%">Price</th>
-                        <th style="text-align:center;" width="5%">Remove</th>
                     </tr>
                     <?php
-                    foreach ($cartItems as $uen => $item) {
+                    foreach ($order_books as $item) {
                         $item_price = $item["quantity"] * $item["price"];
-
-
                     ?>
-
                         <tr>
-                            <td style="border-top: 1px solid black; border-bottom: 1px solid black;"><img src="<?php echo $item["productImage"]; ?>" class="cart-item-image" /><?php echo $item["productName"]; ?></td>
+                            <td style="border-top: 1px solid black; border-bottom: 1px solid black;">
+                                <img src="<?php echo $item["productImage"]; ?>" class="cart-item-image" /><?php echo $item["productName"]; ?>
+                            </td>
                             <td style="text-align:center; border-top: 1px solid black; border-bottom: 1px solid black;"><?php echo "$ " . $item["price"]; ?></td>
                             <td style="text-align:center; border-top: 1px solid black; border-bottom: 1px solid black;"><?php echo $item["quantity"]; ?></td>
+
                             <td style="text-align:center; border-top: 1px solid black; border-bottom: 1px solid black;"><?php echo "$ " . number_format($item_price, 2); ?></td>
-                            <td style="text-align:center; border-top: 1px solid black; border-bottom: 1px solid black;"><a href="cart.php?action=remove&uen=<?php echo $item["bookUEN"]; ?>" class="btnRemoveAction"><img src="images/icon-delete.png" alt="Remove Item" /></a></td>
+
                         </tr>
 
                     <?php
@@ -56,19 +68,28 @@ include "inc/head.inc.php";
                     </tr>
                 </tbody>
             </table>
+        </div>
+        <br>
         <?php
-
-
+        if (!empty($_SESSION["cart_item"])) {
+        ?>
+            <form action="/payment/create-checkout-session.php" method="post">
+                <div class="mb-3" style="padding-bottom:20px;">
+                    <button type="submit" style="float:right;">Proceed to Payment</button>
+                </div>
+            </form>
+        <?php
         }
         ?>
-        <br>
-        <form action="order_summary.php" method="post">
-            <div class="mb-3" style="padding-bottom:20px;">
-                <button type="submit" style="float:right;">Confirm Checkout</button>
-            </div>
-        </form>
+
     </main>
     <?php
     include "inc/footer.inc.php";
     ?>
 </body>
+<style>
+    .overflow-auto {
+        overflow: auto;
+        max-height: 50%;
+    }
+</style>
