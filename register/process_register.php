b82adb82757d86cd00f280be643d5c9f9cdc374e
dennytan-19 <54209319+dennytan-19@users.noreply.github.com>
2024-04-05 19:16:33 +0800

    don't register user if recaptcha score too low

--- register/process_register.php
+++ register/process_register.php
@@ -9,13 +9,13 @@ $fields = array(
     "pwd_confirm" => "Password Confirmation"
 );
 $success = true;
-$is_bot = true; 
-$pwd= $_POST['pwd'];
+$is_bot = true;
+$pwd = $_POST['pwd'];
 $pwd_confirm = $_POST['pwd_confirm'];
 
 // I don't think the catcha works.
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['recaptcha_response'])) {
-    
+
     // Build POST request:
     $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
     $recaptcha_secret = '6Le0e7MZAAAAAI-pCmb3uScqvJUf5y6RN6bTqra4';
@@ -39,70 +39,65 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['recaptcha_response']
     }
 }
 if (!$is_bot) {
+    foreach ($fields as $field => $fieldname) {
+        if (empty($_POST[$field])) {
+            $errorMsg = $fieldname . "is required <br />";
+            $success = false;
+        } else {
+            $email = sanitize_input($_POST["email"]);
+            // Additional check to make sure e-mail address is well-formed.
+            $lname = sanitize_input($_POST["lname"]);
+            $fname = sanitize_input($_POST["fname"]);
 
-} 
+            if ($field === "email" && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $errorMsg2 .= "Invalid email format.";
+                $success = false;
+            }
 
-foreach ($fields as $field => $fieldname) {
-    if (empty($_POST[$field])) {
-        $errorMsg = $fieldname . "is required <br />";
+            if (isset($_POST["user_privilege"])) {
+                $userPrivilege = sanitize_input($_POST["user_privilege"]);
+            } else {
+                $userPrivilege = 'user';
+            }
+        }
+    }
+
+    if ($pwd !== $pwd_confirm) {
+        $errorMsg .= "Passwords do not match.";
+        $success = false;
+    } else {
+        $pwd = password_hash($_POST["pwd"], PASSWORD_DEFAULT);
+    }
+    if (!preg_match('/^[a-zA-Z\-\' ]+$/', $lname)) {
+        $errorMsg .= "Invalid last name format.";
         $success = false;
     }
-    else
-    {
-        $email = sanitize_input($_POST["email"]);
-        // Additional check to make sure e-mail address is well-formed.
-        $lname = sanitize_input($_POST["lname"]);
-        $fname = sanitize_input($_POST["fname"]);
+    if ($success) {
+        saveMemberToDB();
+        // registration successful.
+    }
 
-        if ($field === "email" && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $errorMsg2 .= "Invalid email format.";
-            $success = false;
-        }
-
-        if (isset($_POST["user_privilege"])) {
-            $userPrivilege = sanitize_input($_POST["user_privilege"]);
-        }
-        else {
-            $userPrivilege = 'user';
-        }
-   }
-}
-
-if ($pwd !== $pwd_confirm) {
-    $errorMsg .= "Passwords do not match.";
-    $success = false;
-}
-else {
-    $pwd = password_hash($_POST["pwd"], PASSWORD_DEFAULT);
-}
-if (!preg_match('/^[a-zA-Z\-\' ]+$/', $lname)) {
-    $errorMsg .= "Invalid last name format.";
-    $success = false;
-}
-if ($success) {
-    saveMemberToDB();
-    // registration successful.
-}
-
-if(!$success) {
-    header('Location: ./'. $_POST['prevpage'] .'?errMsg=' . urlencode($errorMsg));
-    exit;
+    if (!$success) {
+        header('Location: ./' . $_POST['prevpage'] . '?errMsg=' . urlencode($errorMsg));
+        exit;
+    }
 }
 
 function sanitize_input($data)
-    {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-function saveMemberToDB() {
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+function saveMemberToDB()
+{
     global $fname, $lname, $email, $pwd, $errorMsg, $success, $userPrivilege;
     //create db connection
     $config_file = '/var/www/private/db-config.ini';
 
     if (file_exists($config_file)) {
-     // Parse the INI file
+        // Parse the INI file
         $config = parse_ini_file($config_file);
     } else {
         // Get configuration from environment variables
@@ -111,19 +106,18 @@ function saveMemberToDB() {
         $config['password'] = getenv('DB_PASSWORD');
         $config['dbname'] = getenv('DBNAME');
     }
-    
+
     $conn = new mysqli(
-        $config['servername'] ,
-        $config['username'] , 
-        $config['password'] , 
-        $config['dbname'] 
-    ); 
+        $config['servername'],
+        $config['username'],
+        $config['password'],
+        $config['dbname']
+    );
 
     if ($conn->connect_error) {
-        $errorMsg = "Connection failed: " .$conn->connect_error;
+        $errorMsg = "Connection failed: " . $conn->connect_error;
         $success = false;
-    }
-    else {
+    } else {
         // Check if the email already exists
         $sql = "SELECT COUNT(*) AS count FROM userTable WHERE email = ?";
         $stmt = $conn->prepare($sql);
@@ -139,17 +133,16 @@ function saveMemberToDB() {
             // If the count is 0, the email does not exist. so can add.
             $stmt = $conn->prepare("INSERT INTO userTable (fName, lName, email, password, userPrivilege) VALUES (?,?,?,?,?)");
             $stmt->bind_param("sssss", $fname, $lname, $email, $pwd, $userPrivilege);
-            if (!$stmt->execute()){
-                $errorMsg = "Execute failed: (" .$stmt->errno .") " . $stmt->error;
+            if (!$stmt->execute()) {
+                $errorMsg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
                 $success = false;
             }
             $stmt->close();
             $conn->close();
-            
+
             // redirect after inserting
             header("Location: /index.php");
             exit;
-            
         }
         $conn->close();
     }
