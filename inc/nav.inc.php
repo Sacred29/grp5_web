45a9be80c6875c19d22849b7d4496960072c535e
Daniel <70926662+Sacred29@users.noreply.github.com>
2024-04-05 22:31:27 +0800

    nav

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-md" style="background-color: #D3D3D3;">
-    <button id="test" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <button id="whole-bar" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="container-fluid">
@@ -58,7 +58,7 @@
 </nav>
 <script>
     var navbar = document.querySelectorAll('.navbar-nav');
-    document.getElementById("test").addEventListener("click", function() {
+    document.getElementById("whole-bar").addEventListener("click", function() {
         navbar.forEach(function(navitem) {
             navitem.classList.toggle("collapse");
         });
